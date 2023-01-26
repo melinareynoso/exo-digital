c157ca08b32fe8bac52f8b8f454c2fea5f5cda7f
Lean <lean@lean.com>
2023-01-26 00:29:41 -0300

    26/1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to hola reclutador que me viene a reclutar.
         </p>
         <a
           className="App-link"
